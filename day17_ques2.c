bd7fc60e7aa4a766d5395ad835f97b150777a271
tirth-pandey <tirthpandey1008@gmail.com>
2026-09-02 08:23:52 +0530

    Add files via upload

---
+++ day17_ques2.c
@@ -0,0 +1,25 @@
+//Write a program to check if a number is prime.
+#include <stdio.h>
+ int main(){
+ int num, i, isPrime=1;
+ printf("Enter a positive integer: ");
+ scanf("%d",&num);  
+  if(num<=1){
+    isPrime=0;
+  }
+ else{
+  for(i=2; i<= num/2; ++i){
+    
+   if(num % i==0){
+       isPrime =0;
+       break;  
+    }
+  }
+ }
+   if (isPrime == 1) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+ return 0;
+}
