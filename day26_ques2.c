378902fa820f1b3e2d313a01ee3b845246cc0908
tirth-pandey <tirthpandey1008@gmail.com>
2026-09-06 16:12:56 +0530

    Add files via upload

---
+++ day26_ques2.c
@@ -0,0 +1,38 @@
+/* Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*  */
+#include <stdio.h>
+int main(){
+   int  i, j, n;
+   for(i = 1;i <= 5;i++){
+   
+     if(i <= 3){
+       n = 2 * i -1;
+      }
+     else{
+      n = 2  * (5 - i) - 1;
+      }
+    for(j = 1; j <= n; j++){
+      printf("*\n");
+      }
+      printf("\n");
+     }
+   return 0;
+  }
+ 
