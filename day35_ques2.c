336bb87b719ade2ba09f2d63c1db89d0ba52cc9b
tirth-pandey <tirthpandey1008@gmail.com>
2026-09-17 22:03:14 +0530

    Add files via upload

---
+++ day35_ques2.c
@@ -0,0 +1,41 @@
+//Rotate an array to the right by k positions.
+#include <stdio.h>
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+void rotateRight(int arr[], int n, int k) {
+    k = k % n; 
+    reverse(arr, 0, n - 1);
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, n - 1);
+}
+
+int main() {
+    int n, k;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the number of positions to rotate (k): ");
+    scanf("%d", &k);
+
+    rotateRight(arr, n, k);
+
+    printf("Rotated array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
