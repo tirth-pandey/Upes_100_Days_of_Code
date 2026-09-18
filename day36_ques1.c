ff4940c01944d0dbecaf733ae8363b163cd6c993
tirth-pandey <tirthpandey1008@gmail.com>
2026-09-18 11:25:07 +0530

    Add files via upload

---
+++ day36_ques1.c
@@ -0,0 +1,25 @@
+//Read and print a matrix.
+#include <stdio.h>
+int main()
+{
+ int rows, cols;
+ printf("Enter no of rows: ");
+ scanf("%d", &rows);
+ printf("Enter no of columns: ");
+ scanf("%d", &cols);
+ int matrix[rows] [cols];
+ for(int i=0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+    scanf("%d",&matrix[i][j]);
+   }
+ }
+   printf("The matrix is:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf(" %d ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
